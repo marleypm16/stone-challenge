9e9e9ad004ba39a569972856e9f4d46e3ad23af0
marleypm16 <yarlempm16@gmail.com>
2024-05-10 20:56:28 -0300

    feat: inital commit

---
+++ app/_components/valueConverter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ClientForm} from "@/app/_lib/clientForm";
+interface ValueConverterProps{
+    clientForm : ClientForm
+}
+const ValueConverter = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ValueConverter;
