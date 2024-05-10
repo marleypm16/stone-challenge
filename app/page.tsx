d07b61992cf8d7769bf0b6f5d16f55a67e7a9334
marleypm16 <yarlempm16@gmail.com>
2024-05-10 20:56:20 -0300

    feat: function for capture client submit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
-import Image from "next/image";
+"use client"
 import Header from "@/app/_components/header";
 import Form from "@/app/_components/form";
+import {ClientForm} from "@/app/_lib/clientForm";
+import {useState} from "react";
 
 export default function Home() {
+    const [clientForm, setClientForm] = useState<ClientForm>({
+        dolar: 1,
+        taxa: 0,
+        tipo: ''
+
+    })
+    const onSubmit = (clientForm : ClientForm) : void =>{
+        setClientForm(clientForm)
+    }
   return (
       <>
         <Header/>
-        <Form/>
+        <Form clientForm={clientForm} onSubmit={onSubmit} />
+
       </>
   );
 }
