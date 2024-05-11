8ad0bb4dd214fa89ee7a5d4a98cbc3fa4b9b400e
marleypm16 <yarlempm16@gmail.com>
2024-05-11 13:23:28 -0300

    feat : add styles

--- app/_components/converterPage.tsx
+++ app/_components/converterPage.tsx
@@ -3,6 +3,8 @@ import React, {useEffect, useState} from 'react';
 import {ClientForm} from "@/app/_lib/clientForm";
 import {api} from "@/app/_lib/getApi";
 import {calculateFinalValue} from "@/app/_lib/calculateFinalValue";
+import {Button} from "primereact/button";
+import {formatValue} from "@/app/_lib/formatValue";
 interface ConverterPageProps{
     clientForm : ClientForm
     goBack : () => void
@@ -30,9 +32,17 @@ const ConverterPage =  ({clientForm,goBack} : ConverterPageProps) => {
 
     return (
         <div>
-            <button onClick={handleGoBack}> Voltar</button>
-            <h1>O resultado do calculo é :</h1>
-            <h2>{calculateFinalValue(dolar,taxa,tipo,Number(cotacao))}</h2>
+            <Button className='p-4 bg-transparent text-black border shadow-goBackButton rounded-lg mb-5' onClick={handleGoBack}>  Voltar</Button>
+            <div className="flex flex-col justify-center gap-4">
+                <h1 className='text-form font-bold text-xl '>O resultado do calculo é :</h1>
+                <h2 className='font-bold text-green-500 text-6xl'>{formatValue(calculateFinalValue(dolar, taxa, tipo, Number(cotacao)),'BRL')}</h2>
+                <div>
+                    <p className="text-header text-sm mb-2">Compra no {tipo} com taxa de {taxa}%</p>
+                    <p className="text-header text-sm">Cotação do dólar : {formatValue(cotacao!,'USD')}</p>
+                </div>
+
+            </div>
+
 
         </div>
     );
