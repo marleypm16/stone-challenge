aaa1fa53cb7f0ae1cc0492c4d30307282f2e656b
marleypm16 <yarlempm16@gmail.com>
2024-05-10 23:10:59 -0300

    feat : input from primeReact

--- app/_components/form.tsx
+++ app/_components/form.tsx
@@ -1,14 +1,14 @@
 "use client"
 import React, {useState} from 'react';
 import {ClientForm} from "@/app/_lib/clientForm";
-
+import { InputNumber } from 'primereact/inputnumber';
 interface FormProps{
     clientForm : ClientForm
     onSubmit : (clientForm : ClientForm) => void
 }
 
 const Form = ({onSubmit} : FormProps) => {
-    const [dolar,setDoalr] = useState(1)
+    const [dolar,setDolar] = useState(0)
     const [taxa,setTaxa] = useState(0)
     const [tipo,setTipo] = useState('')
     const handleSubmit = (e :React.FormEvent) : void =>{
@@ -16,21 +16,29 @@ const Form = ({onSubmit} : FormProps) => {
         onSubmit({dolar,taxa,tipo})
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="dolar">Dolar</label>
-                <input type="number" id="dolar" onChange={(e)=> setDoalr(Number(e.target.value))} value={dolar} name="name"/>
-                <label htmlFor="taxa">Taxa do Estado</label>
-                <input type="number" onChange={(e)=> setTaxa(Number(e.target.value))} value={taxa} id="taxa" name="name"/>
+        <form className='w-1/2' onSubmit={handleSubmit}>
+            <div className="flex items-center gap-5 mb-6">
+                <div className="w-1/2">
+                    <label className='block mb-2' htmlFor="dolar">Dolar</label>
+                    <InputNumber  minFractionDigits={2} className="" id="dolar" onChange={(e) => setDolar(Number(e.value))} placeholder='0' required name="name"/>
+                </div>
+                <div className="w-1/2">
+                    <label className='block mb-2' htmlFor="taxa">Taxa do Estado</label>
+                    <InputNumber  className="w-full" minFractionDigits={2}  id="taxa" onChange={(e) => setTaxa(Number(e.value))} placeholder='0' required name="name"/>
+                </div>
             </div>
-            <div>
-                <h2>Tipo de compra</h2>
-                <input type="radio" id="dinheiro" name="tipo" onChange={e => setTipo(e.target.value)} value="dinheiro" />
-                <label htmlFor="dinheiro">Dinheiro</label>
-                <input type="radio" id="cartao" name="tipo" onChange={e => setTipo(e.target.value)} value="cartao"/>
-                <label htmlFor="cartao">Cartão</label>
+            <div className="mb-6">
+                <h2 className="mb-2">Tipo de compra</h2>
+                <div className='flex items-center gap-4'>
+                    <input type="radio" id="dinheiro" name="tipo" onChange={e => setTipo(e.target.value)}
+                           value="dinheiro" required/>
+                    <label htmlFor="dinheiro">Dinheiro</label>
+                    <input required className='checked:text-green-500' type="radio" id="cartao" name="tipo" onChange={e => setTipo(e.target.value)} value="cartao"/>
+                    <label htmlFor="cartao">Cartão</label>
+
+                </div>
             </div>
-            <button type='submit'>Converter</button>
+            <button className='px-5 py-4 bg-green-500 text-white rounded-2xl' type='submit'>Converter</button>
         </form>
     );
 };
